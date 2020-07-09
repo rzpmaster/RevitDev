de7f0ab1e438288f1e9fe55367a33e1d157adfd1
rzpmaster <1019394597@qq.com>
2020-07-09 23:02:45 +0800

    BinaryReaderDemo

--- DotNetLibraries/StreamDemo/BinaryReaderDemo.cs
+++ DotNetLibraries/StreamDemo/BinaryReaderDemo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,22 @@ namespace StreamDemo
     /// </summary>
     class BinaryReaderDemo
     {
+        public void WriteFileUsingBinaryWriter()
+        {
+            var outputStream = File.Create(tempTextFileName);
+            using (var writer = new BinaryWriter(outputStream))
+            {
+                double d = 47.47;
+                int i = 42;
+                long l = 987654321;
+                string s = "sample";
+                writer.Write(d);
+                writer.Write(i);
+                writer.Write(l);
+                writer.Write(s);
+            }
+        }
+
+        string tempTextFileName = @"C:\Users\rzp\Desktop\temp.txt";
     }
 }
