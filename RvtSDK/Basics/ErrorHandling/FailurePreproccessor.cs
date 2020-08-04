6e87b51c2ccf1cbfc974b67b4fd0471df309b17a
rzp <1019394597@qq.com>
2020-08-04 14:01:10 +0800

    asyndemo

---
+++ RvtSDK/Basics/ErrorHandling/FailurePreproccessor.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    class FailurePreproccessor : IFailuresPreprocessor
+    {
+        /// <summary>
+        /// 当事务结束时发现故障，并且Revit即将开始处理这些故障时，将调用此方法
+        /// </summary>
+        /// <param name="failuresAccessor"></param>
+        /// <returns></returns>
+        public FailureProcessingResult PreprocessFailures(FailuresAccessor failuresAccessor)
+        {
+            IList<FailureMessageAccessor> fmas = failuresAccessor.GetFailureMessages();
+            if (fmas.Count == 0)
+            {
+                return FailureProcessingResult.Continue;
+            }
+
+            String transactionName = failuresAccessor.GetTransactionName();
+            if (transactionName.Equals("Warning_FailurePreproccessor"))
+            {
+                foreach (FailureMessageAccessor fma in fmas)
+                {
+                    FailureDefinitionId id = fma.GetFailureDefinitionId();
+                    if (id == Command.m_idWarning)
+                    {
+                        failuresAccessor.DeleteWarning(fma);
+                    }
+                }
+
+                return FailureProcessingResult.ProceedWithCommit;
+            }
+            else if (transactionName.Equals("Warning_FailurePreproccessor_OverlappedWall"))
+            {
+                foreach (FailureMessageAccessor fma in fmas)
+                {
+                    FailureDefinitionId id = fma.GetFailureDefinitionId();
+                    if (id == BuiltInFailures.OverlapFailures.WallsOverlap)
+                    {
+                        failuresAccessor.DeleteWarning(fma);
+                    }
+                }
+
+                return FailureProcessingResult.ProceedWithCommit;
+            }
+            else
+            {
+                return FailureProcessingResult.Continue;
+            }
+        }
+    }
+}
---
+++ RvtSDK/Basics/ErrorHandling/FailuresProcessor.cs
@@ -0,0 +1,52 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    class FailuresProcessor : IFailuresProcessor
+    {
+        /// <summary>
+        /// 在发生异常或文档销毁时调用此方法，以消除可能留在屏幕上的任何可能挂起的失败UI
+        /// </summary>
+        /// <param name="document"></param>
+        public void Dismiss(Document document)
+        {
+        }
+
+        /// <summary>
+        /// Revit将在事务结束时处理失败时调用该方法。
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public FailureProcessingResult ProcessFailures(FailuresAccessor failuresAccessor)
+        {
+            IList<FailureMessageAccessor> fmas = failuresAccessor.GetFailureMessages();
+            if (fmas.Count == 0)
+            {
+                return FailureProcessingResult.Continue;
+            }
+
+            String transactionName = failuresAccessor.GetTransactionName();
+            if (transactionName.Equals("Error_FailuresProcessor"))
+            {
+                foreach (FailureMessageAccessor fma in fmas)
+                {
+                    FailureDefinitionId id = fma.GetFailureDefinitionId();
+                    if (id == Command.m_idError)
+                    {
+                        failuresAccessor.ResolveFailure(fma);
+                    }
+                }
+                return FailureProcessingResult.ProceedWithCommit;
+            }
+            else
+            {
+                return FailureProcessingResult.Continue;
+            }
+        }
+    }
+}
