363061a79b4417977c5fa9468c4918406e6750f3
rzp <1019394597@qq.com>
2020-08-10 21:20:04 +0800

    0.0

---
+++ DotNetLibraries/Log4NetDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4NetDemo
+{
+    class Program
+    {
+        //static readonly log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static readonly log4net.ILog log = LogHelper.GetLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("log4net demo");
+
+            log.Error("this is my error message.");
+
+            Console.ReadKey();
+        }
+    }
+}
