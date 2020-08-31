9152463833553453b88a6b56023b8b5262d5897e
rzp <1019394597@qq.com>
2020-08-31 11:37:08 +0800

    add a new sdk sample

---
+++ RvtSDK/Tests/FailureDefinitionIdMap/Command.cs
@@ -0,0 +1,67 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FailureDefinitionIdMap
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Command()
+        {
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new TextWriterTraceListener(@"C:\Users\rzp\Desktop\BuiltInFailuresIdMaps.txt"));
+            Trace.AutoFlush = true;
+        }
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            DataSet dataSet = new DataSet();
+
+            var type = typeof(BuiltInFailures);
+
+            var nestedTypes = type.GetNestedTypes();
+
+            foreach (var tp in nestedTypes)
+            {
+                Trace.WriteLine(tp.Name);
+                Trace.Indent();
+
+                DataTable dt = new DataTable(tp.Name);
+                dataSet.Tables.Add(dt);
+
+                DataColumn[] columns = new DataColumn[]
+                {
+                    new DataColumn("Name"),
+                    new DataColumn("Guid")
+                };
+                dt.Columns.AddRange(columns);
+
+                var props = tp.GetProperties();
+                foreach (var prop in props)
+                {
+                    Trace.WriteLine(string.Format("{0}{1}{2}",
+                        prop.Name.PadRight(55),
+                        "\t",
+                        (prop.GetValue(null) as FailureDefinitionId).Guid));
+
+                    DataRow dataRow = dt.NewRow();
+                    dataRow[0] = prop.Name;
+                    dataRow[1] = (prop.GetValue(null) as FailureDefinitionId).Guid;
+                    dt.Rows.Add(dataRow);
+                }
+
+                Trace.Unindent();
+            }
+
+            return Result.Succeeded;
+        }
+
+    }
+}
--- RvtSDK/Views/DuplicateViews/DuplicateAcrossDocumentsCommand.cs
+++ RvtSDK/Views/DuplicateViews/DuplicateAcrossDocumentsCommand.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DuplicateViews
 {
-    class DuplicateAcrossDocumentsCommand
+    /// <summary>
+    /// 复制所有可复制的绘图视图和明细表
+    /// </summary>
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class DuplicateAcrossDocumentsCommand : IExternalCommand
     {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Autodesk.Revit.ApplicationServices.Application application = commandData.Application.Application;
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            // Find target document - it must be the only other open document in session
+            Document toDocument = null;
+            IEnumerable<Document> documents = application.Documents.Cast<Document>();
+            if (documents.Count<Document>() != 2)
+            {
+                TaskDialog.Show("No target document",
+                                "This tool can only be used if there are two documents (a source document and target document).");
+                return Result.Cancelled;
+            }
+            foreach (Document loadedDoc in documents)
+            {
+                if (loadedDoc.Title != doc.Title)
+                {
+                    toDocument = loadedDoc;
+                    break;
+                }
+            }
+
+            // Collect schedules and drafting views
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+            List<Type> viewTypes = new List<Type>();
+            viewTypes.Add(typeof(ViewSchedule));    // 明细表视图
+            viewTypes.Add(typeof(ViewDrafting));    // 绘图试图
+            ElementMulticlassFilter filter = new ElementMulticlassFilter(viewTypes);
+            collector.WherePasses(filter);
+
+            // skip view-specfic schedules (e.g. Revision Schedules);
+            // These should not be copied as they are associated to another view that cannot be copied
+            collector.WhereElementIsViewIndependent(); 
+            
+
+            // Copy all schedules together so that any dependency elements are copied only once
+            IEnumerable<ViewSchedule> schedules = collector.OfType<ViewSchedule>();
+            DuplicateViewUtils.DuplicateSchedules(doc, schedules, toDocument);
+            int numSchedules = schedules.Count<ViewSchedule>();
+
+            // Copy drafting views together
+            IEnumerable<ViewDrafting> draftingViews = collector.OfType<ViewDrafting>();
+            int numDraftingElements =
+                DuplicateViewUtils.DuplicateDraftingViews(doc, draftingViews, toDocument);
+            int numDrafting = draftingViews.Count<ViewDrafting>();
+
+            // Show results
+            TaskDialog.Show("Statistics",
+                   String.Format("Copied: \n" +
+                                "\t{0} schedules.\n" +
+                                "\t{1} drafting views.\n" +
+                                "\t{2} new drafting elements created.",
+                   numSchedules, numDrafting, numDraftingElements));
+
+            return Result.Succeeded;
+        }
     }
 }
